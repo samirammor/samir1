c6da48e7d0d91411627759092594bd34efc8e683
samirammor <Samir.Ammor@realdolmen.com>
2017-10-09 12:46:37 +0200

    Added initial sources

---
+++ src/main/java/com/realdolmen/App.java
@@ -0,0 +1,21 @@
+package com.realdolmen;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static Logger logger=LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	logger.info("hey");
+        //System.out.println( "Hello!" );
+    	
+    }
+}
